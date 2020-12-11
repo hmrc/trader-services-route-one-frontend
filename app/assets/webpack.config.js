64a745604e22938314af9c493832a660d9c8235f
Marcin Cichon <slashrealm@gmail.com>
2020-12-11 21:07:14 +0000

    [DOR-196] Set up ESLint

--- app/assets/webpack.config.js
+++ app/assets/webpack.config.js
@@ -10,13 +10,19 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules|vendor/,
+      //   loader: 'eslint-loader'
+      // }
     ]
   },
   output: {
