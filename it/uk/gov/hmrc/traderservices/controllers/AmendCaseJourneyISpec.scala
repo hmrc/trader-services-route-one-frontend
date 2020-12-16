c7ea6ef0369b97f23bc995ccad3204c0b5a7914b
Sweta Chandak <sweta.chandak@digital.hmrc.gov.uk>
2020-12-16 12:01:32 +0000

    [SC] Increasing code coverage for LanguageController

--- it/uk/gov/hmrc/traderservices/controllers/AmendCaseJourneyISpec.scala
+++ it/uk/gov/hmrc/traderservices/controllers/AmendCaseJourneyISpec.scala
@@ -1,30 +1,17 @@
 package uk.gov.hmrc.traderservices.controllers
 
-import java.util.UUID
-
-import play.api.Application
-import play.api.libs.json.Format
-import play.api.libs.ws.WSClient
-import play.api.mvc.{Cookies, Session, SessionCookieBaker}
-import uk.gov.hmrc.cache.repository.CacheMongoRepository
-import uk.gov.hmrc.crypto.{ApplicationCrypto, PlainText}
-import uk.gov.hmrc.play.bootstrap.frontend.filters.crypto.SessionCookieCrypto
-import uk.gov.hmrc.traderservices.journeys.AmendCaseJourneyStateFormats
 import uk.gov.hmrc.traderservices.models._
-import uk.gov.hmrc.traderservices.services.{AmendCaseJourneyService, MongoDBCachedJourneyService}
 import uk.gov.hmrc.traderservices.stubs.{TraderServicesApiStubs, UpscanInitiateStubs}
-import uk.gov.hmrc.traderservices.support.{ServerISpec, TestJourneyService}
+import uk.gov.hmrc.traderservices.support.ServerISpec
 
+import java.time.{LocalDateTime, ZonedDateTime}
 import scala.concurrent.ExecutionContext.Implicits.global
-import java.time.LocalDateTime
-import java.time.ZonedDateTime
 import scala.util.Random
-import uk.gov.hmrc.traderservices.wiring.AppConfig
 
-class AmendCaseJourneyISpec extends AmendCaseJourneyISpecSetup with TraderServicesApiStubs with UpscanInitiateStubs {
+class AmendCaseJourneyISpec extends ServerISpec with TraderServicesApiStubs with UpscanInitiateStubs {
 
-  import journey.model.State._
   import journey.model.FileUploadState._
+  import journey.model.State._
 
   val dateTime = LocalDateTime.now()
 
@@ -389,50 +376,4 @@ class AmendCaseJourneyISpec extends AmendCaseJourneyISpecSetup with TraderServic
       }
     }
   }
-}
-
-trait AmendCaseJourneyISpecSetup extends ServerISpec {
-
-  override def fakeApplication: Application = appBuilder.build()
-
-  lazy val wsClient: WSClient = app.injector.instanceOf[WSClient]
-  lazy val appConfig = fakeApplication.injector.instanceOf[AppConfig]
-  lazy val sessionCookieBaker: SessionCookieBaker = app.injector.instanceOf[SessionCookieBaker]
-  lazy val sessionCookieCrypto: SessionCookieCrypto = app.injector.instanceOf[SessionCookieCrypto]
-
-  case class JourneyId(value: String = UUID.randomUUID().toString)
-
-  // define test service capable of manipulating journey state
-  lazy val journey = new TestJourneyService[JourneyId]
-    with AmendCaseJourneyService[JourneyId] with MongoDBCachedJourneyService[JourneyId] {
-
-    override lazy val cacheMongoRepository = app.injector.instanceOf[CacheMongoRepository]
-    override lazy val applicationCrypto = app.injector.instanceOf[ApplicationCrypto]
-
-    override val stateFormats: Format[model.State] =
-      AmendCaseJourneyStateFormats.formats
-
-    override def getJourneyId(journeyId: JourneyId): Option[String] = Some(journeyId.value)
-  }
-
-  val baseUrl: String = s"http://localhost:$port/send-documents-for-customs-check"
-
-  def request(path: String)(implicit journeyId: JourneyId) = {
-    val sessionCookie = sessionCookieBaker.encodeAsCookie(Session(Map(journey.journeyKey -> journeyId.value)))
-
-    wsClient
-      .url(s"$baseUrl$path")
-      .withHttpHeaders(
-        play.api.http.HeaderNames.COOKIE -> Cookies.encodeCookieHeader(
-          Seq(
-            sessionCookie.copy(value = sessionCookieCrypto.crypto.encrypt(PlainText(sessionCookie.value)).value)
-          )
-        )
-      )
-  }
-
-  def requestWithoutJourneyId(path: String) =
-    wsClient
-      .url(s"$baseUrl$path")
-
 }
