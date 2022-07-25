509efa045276c2c22b1cd9ec1622590863123035
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2022-07-25 15:59:30 +0100

    DDCYLS-1166: update hmrc mongo lin to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object AppDependencies {
   lazy val compileDeps = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "5.24.0",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "6.4.0",
     "uk.gov.hmrc"                  %% "auth-client"                % "5.8.0-play-28",
     "uk.gov.hmrc"                  %% "play-fsm"                   % "0.89.0-play-28",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.63.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.68.0",
     "uk.gov.hmrc"                  %% "json-encryption"            % "4.11.0-play-28",
     "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.15.0-play-28",
     "com.googlecode.libphonenumber" % "libphonenumber"             % "8.12.31",
