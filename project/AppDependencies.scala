ea0ecbfdd47155671b74a5cbb9bded2306dc9b65
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2025-04-03 16:53:32 +0100

    DDCYLS-6309 updating

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% s"play-frontend-hmrc-play-$playVersion"    % "8.5.0",
     "uk.gov.hmrc.mongo"             %% s"hmrc-mongo-play-$playVersion"            % mongoVersion,
     "uk.gov.hmrc"                   %% s"crypto-json-play-$playVersion"           % "7.6.0",
-    "com.googlecode.libphonenumber" % "libphonenumber"                            % "8.13.26",
+    "com.googlecode.libphonenumber" % "libphonenumber"                            % "8.13.47",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"                     % "2.14.2",
     "com.sun.mail"                  % "javax.mail"                                % "1.6.2",
     "commons-codec"                 % "commons-codec"                             % "1.10"
@@ -18,11 +18,11 @@ object AppDependencies {
 
   val testDeps: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-play-$playVersion"        % bootstrapVersion,
-    "org.mockito"            %% "mockito-scala-scalatest"                  % "1.17.29",
-    "org.scalatestplus"      %% "scalacheck-1-17"                          % "3.2.17.0",
+    "org.mockito"            %% "mockito-scala-scalatest"                  % "1.17.37",
+    "org.scalatestplus"      %% "scalacheck-1-17"                          % "3.2.18.0",
     "com.vladsch.flexmark"   %  "flexmark-all"                             % "0.64.8",
     "org.scalameta"          %% "munit"                                    % "0.7.29",
-    "org.scalacheck"         %% "scalacheck"                               % "1.17.0"
+    "org.scalacheck"         %% "scalacheck"                               % "1.18.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
