8f75017d1295ec036f199046f5a096429581cd45
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2025-06-27 13:59:20 +0100

    Platform upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   lazy val compileDeps = Seq(
     "uk.gov.hmrc"                   %% s"bootstrap-frontend-play-$playVersion"    % bootstrapVersion,
-    "uk.gov.hmrc"                   %% s"play-frontend-hmrc-play-$playVersion"    % "12.1.0",
+    "uk.gov.hmrc"                   %% s"play-frontend-hmrc-play-$playVersion"    % "12.6.0",
     "uk.gov.hmrc.mongo"             %% s"hmrc-mongo-play-$playVersion"            % "2.6.0",
     "uk.gov.hmrc"                   %% s"crypto-json-play-$playVersion"           % "8.2.0",
     "com.googlecode.libphonenumber" % "libphonenumber"                            % "8.13.47",
