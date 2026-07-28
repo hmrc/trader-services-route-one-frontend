b0ade256b39212e8a1cf3b61831c7cac554bfb27
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2026-07-28 15:39:21 +0100

    DDCYLS-8871 Upgrading dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,9 +13,9 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% s"play-frontend-hmrc-play-$playVersion"    % "13.9.0",
     "uk.gov.hmrc.mongo"             %% s"hmrc-mongo-play-$playVersion"            % "2.12.0",
     "uk.gov.hmrc"                   %% s"crypto-json-play-$playVersion"           % "8.4.0",
-    "com.googlecode.libphonenumber" % "libphonenumber"                            % "8.13.47",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                     % "2.19.0",
-    "commons-codec"                 % "commons-codec"                             % "1.10"
+    "com.googlecode.libphonenumber" % "libphonenumber"                            % "9.0.35",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                     % "2.22.1",
+    "commons-codec"                 % "commons-codec"                             % "1.22.0"
   ) ++ mailDependencies
 
   val testDeps: Seq[ModuleID] = (Seq(
@@ -23,7 +23,7 @@ object AppDependencies {
     "org.scalatestplus"      %% "scalacheck-1-17"                          % "3.2.18.0",
     "com.vladsch.flexmark"   %  "flexmark-all"                             % "0.64.8",
     "org.scalameta"          %% "munit"                                    % "0.7.29",
-    "org.scalacheck"         %% "scalacheck"                               % "1.18.1"
+    "org.scalacheck"         %% "scalacheck"                               % "1.19.0"
   ) ++ mailDependencies).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
