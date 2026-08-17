b1c0e517b39d80159428324dea5a3a2d5c58511c
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-08-17 09:25:21 +0100

    DDCYLS-8903: removing flex mark dependency (#497)
    * removing flex mark dependency
    
    * updating play frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   lazy val compileDeps = Seq(
     "uk.gov.hmrc"                   %% s"bootstrap-frontend-play-$playVersion"    % bootstrapVersion,
-    "uk.gov.hmrc"                   %% s"play-frontend-hmrc-play-$playVersion"    % "13.9.0",
+    "uk.gov.hmrc"                   %% s"play-frontend-hmrc-play-$playVersion"    % "13.10.0",
     "uk.gov.hmrc.mongo"             %% s"hmrc-mongo-play-$playVersion"            % "2.13.0",
     "uk.gov.hmrc"                   %% s"crypto-json-play-$playVersion"           % "8.4.0",
     "com.googlecode.libphonenumber" % "libphonenumber"                            % "9.0.35",
@@ -21,7 +21,6 @@ object AppDependencies {
   val testDeps: Seq[ModuleID] = (Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-play-$playVersion"        % bootstrapVersion,
     "org.scalatestplus"      %% "scalacheck-1-17"                          % "3.2.18.0",
-    "com.vladsch.flexmark"   %  "flexmark-all"                             % "0.64.8",
     "org.scalameta"          %% "munit"                                    % "0.7.29",
     "org.scalacheck"         %% "scalacheck"                               % "1.19.0"
   ) ++ mailDependencies).map(_ % Test)
